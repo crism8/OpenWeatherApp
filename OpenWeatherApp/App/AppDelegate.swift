24ffa4deb123aecc65e2158febdabe8aafc19163
Cristian <cmakarski@cyfrowypolsat.pl>
2020-09-29 00:37:17 +0200

    project setup

---
+++ OpenWeatherApp/App/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  OpenWeatherApp
+//
+//  Created by Cristian Makarski on 28/09/2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
