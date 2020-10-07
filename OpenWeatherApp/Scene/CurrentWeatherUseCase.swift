1eb4957bc245154dd5203b6fd3de22fe0c6c0d8d
Cristian <cmakarski@cyfrowypolsat.pl>
2020-10-07 04:31:32 +0200

    status obsevable WIP

--- OpenWeatherApp/Scene/CurrentWeatherUseCase.swift
+++ OpenWeatherApp/Scene/CurrentWeatherUseCase.swift
@@ -106,4 +106,63 @@ final class CurrentWeatherUseCase: BaseDataController {
                 onDispose: { [unowned self] in self.locationManager.stopUpdatingLocation() })
     }
     
+    private func requestStatusObservable() -> Observable<CLAuthorizationStatus> {
+        let statusObservable: Observable<CLAuthorizationStatus> = locationManager!
+            .rx
+            .status
+            .filter {
+                $0 == .notDetermined
+            }
+        
+        let didChangeAuthorizationStatus: Observable<CLAuthorizationStatus> = locationManager!
+            .rx
+            .didChangeAuthorization
+            .map { $0.status }
+        return Observable.merge(statusObservable, didChangeAuthorizationStatus)
+    }
+
+//    private func requestStatus() {
+//        requestStatusObservable()
+//            .subscribe({ [weak self] status in
+//                guard let self = self else {
+//                    return
+//                }
+//
+//                switch status.element {
+//                case .denied:
+//                    print("Authorization denied")
+//                    throw AppError.locationError(withCode: .location, message: "Location Authorization denied")
+//                case .notDetermined:
+//                    print("Authorization: not determined")
+//                    throw AppError.locationError(withCode: .location, message: "Location Authorization: not determined")
+//                case .restricted:
+//                    print("Authorization: restricted")
+//                    throw AppError.locationError(withCode: .location, message: "Location Authorization: restricted")
+//                case .authorizedAlways, .authorizedWhenInUse:
+//                    print("All good fire request)")
+//                @unknown default:
+//                    throw AppError.locationError(withCode: .location, message: "Not Handled location status")
+//                }
+//            }).disposed(by: disposeBag)
+//    }
+
+    
+//    private func handle(status: CLAuthorizationStatus) throws {
+//        switch status {
+//        case .denied:
+//            print("Authorization denied")
+//            throw AppError.locationError(withCode: .location, message: "Location Authorization denied")
+//        case .notDetermined:
+//            print("Authorization: not determined")
+//            throw AppError.locationError(withCode: .location, message: "Location Authorization: not determined")
+//        case .restricted:
+//            print("Authorization: restricted")
+//            throw AppError.locationError(withCode: .location, message: "Location Authorization: restricted")
+//        case .authorizedAlways, .authorizedWhenInUse:
+//            print("All good fire request)")
+//        @unknown default:
+//            throw AppError.locationError(withCode: .location, message: "Not Handled location status")
+//        }
+//    }
+    
 }
